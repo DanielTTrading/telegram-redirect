e99c09aff61ce529d2eb3e931ab3f233e176904d
DanielTTrading <dv@ttrading.co>
2026-07-02 18:17:00 -0500

    Redirect a Telegram

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+@app.route('/telegram')
+def telegram_redirect():
+    return redirect("https://t.me/Jptactical_bot", code=302)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
